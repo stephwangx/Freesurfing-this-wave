a30959f9c0972757d5f3164a0426d6f0e2ea4f18
Stephanie Wang <51331611+stephwangx@users.noreply.github.com>
2019-07-30 16:11:34 -0400

    Create v1: confusion.py

---
+++ v1: confusion.py
@@ -0,0 +1,10 @@
+from sklearn.metrics import confusion_matrix
+import pandas as pd
+d = pd.read_csv('Mitnec_Minc_y_true.csv')
+d2 = pd.read_csv('y_pred_M0.83.csv')
+df = pd.DataFrame(data=d)
+df2 = pd.DataFrame(data=d2)
+x = df["Amyloid_Clinical_Reads"].tolist()
+y = df2["MNI_Pipeline_Global_SUVr"].tolist()
+df_confusion = confusion_matrix(df_true, df2_pred)
+print (df_confusion)
